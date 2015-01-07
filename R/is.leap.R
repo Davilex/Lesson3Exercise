1e4fd6c0e4f8c87fd51dc6de2ffec18a9224359c
unknown <schol065@D0117111.wurnet.nl>
2015-01-07 16:28:29 +0100

    test cases added and comments for improving readability

--- R/is.leap.R
+++ R/is.leap.R
@@ -1,15 +1,26 @@
 #Function to check if a year is a leap year.
 is.leap <- function(year) {
+  # check if input is a number
   if (!is.numeric(year)) {
     stop("argument of class numeric expected")
+  
+  # check if input year is before 1582, which is when the Gregorian calender was first used
   } else if (year < 1582) {
     outcome <- paste(year, "is out of the valid range", sep=" ") 
+  
+  # check if input year is divisible by 400, then it is a leap year
   } else if (year %% 400 == 0){
     outcome <- T
+    
+  # check if input year is divisible by 100, then it is a common year
   } else if (year %% 100 == 0) {
     outcome <- F
+    
+  # check if input year is divisible by 4, then it is a leap year    
   } else if (year %% 4 == 0) {
     outcome <- T
+    
+  # otherwise, the input year is a common year
   } else {
     outcome <- F
   } 
--- main.R
+++ main.R
@@ -15,4 +15,19 @@ source("R/is.leap.R")
 
 # Then the actual commands
 #Test cases
-is.leap(2900)
+
+#Test case 1
+#Should return error: "argument of class numeric expected"
+is.leap("john")
+
+#Test case 2
+#Should return "1120 is out of the valid range"
+is.leap(1120)
+
+#Test case 3
+#Should return False
+is.leap(1997)
+
+#Test case 4
+#Should return True
+is.leap(2000)
